044eda8d310233b21ece8d7ae03ec2b23b15e015
Claire Lim <e0559833@u.nus.edu>
2021-06-08 16:27:39 +0800

    add MYSQL scripts for faculty, major

---
+++ backend_orbital/MySQLscripts/insertFaculty.sql
@@ -0,0 +1,11 @@
+INSERT INTO Faculty VALUES ("CHS", "College of Humanities and Sciences");
+INSERT INTO Faculty VALUES ("BIZ", "Business & Accountancy");
+INSERT INTO Faculty VALUES ("COM", "Computing");
+INSERT INTO Faculty VALUES ("DEN", "Dentistry");
+INSERT INTO Faculty VALUES ("DE", "Design & Environment");
+INSERT INTO Faculty VALUES ("ENGR", "Engineering");
+INSERT INTO Faculty VALUES ("LAW", "Law");
+INSERT INTO Faculty VALUES ("MED", "Medicine");
+INSERT INTO Faculty VALUES ("NSG", "Nursing");
+INSERT INTO Faculty VALUES ("PHAR", "Pharmacy");
+INSERT INTO Faculty VALUES ("MUS", "Music");
---
+++ backend_orbital/MySQLscripts/insertMajor.sql
@@ -0,0 +1,77 @@
+# asian studies & humanities
+INSERT INTO MAJOR VALUES ("CL", "Chinese Language", "CHS");
+INSERT INTO MAJOR VALUES ("CSd", "Chinese Studies", "CHS");
+INSERT INTO MAJOR VALUES ("EL", "English Language", "CHS");
+INSERT INTO MAJOR VALUES ("ELit", "English Literature", "CHS");
+INSERT INTO MAJOR VALUES ("HIS", "History", "CHS");
+INSERT INTO MAJOR VALUES ("JSd", "Japanese Studies", "CHS");
+INSERT INTO MAJOR VALUES ("MSd", "Malay Studies", "CHS");
+INSERT INTO MAJOR VALUES ("PHILO", "Philosophy", "CHS");
+INSERT INTO MAJOR VALUES ("SASd", "South Asian Studies", "CHS");
+INSERT INTO MAJOR VALUES ("SEASd", "Southeast Asian Studies", "CHS");
+INSERT INTO MAJOR VALUES ("TSd", "Theatre Studies", "CHS");
+
+# social sciences
+INSERT INTO MAJOR VALUES ("CNM", "Communications and New Media", "CHS");
+INSERT INTO MAJOR VALUES ("ECON", "Economics", "CHS");
+INSERT INTO MAJOR VALUES ("GEOG", "Geography", "CHS");
+INSERT INTO MAJOR VALUES ("GSd", "Global Studies", "CHS");
+INSERT INTO MAJOR VALUES ("PolS", "Political Science", "CHS");
+INSERT INTO MAJOR VALUES ("PSYCH", "Psychology", "CHS");
+INSERT INTO MAJOR VALUES ("SW", "Social Work", "CHS");
+INSERT INTO MAJOR VALUES ("SOCIO", "Sociology", "CHS");
+
+# sciences
+INSERT INTO MAJOR VALUES ("CHEM", "Chemistry", "CHS");
+INSERT INTO MAJOR VALUES ("DSA", "Data Science and Analytics", "CHS");
+INSERT INTO MAJOR VALUES ("FST", "Food Science and Technology", "CHS");
+INSERT INTO MAJOR VALUES ("LS", "Life Sciences", "CHS");
+INSERT INTO MAJOR VALUES ("MATH", "Mathematics", "CHS");
+INSERT INTO MAJOR VALUES ("PharS", "Pharmaceutical Science", "CHS");
+INSERT INTO MAJOR VALUES ("PHYS", "Physics", "CHS");
+INSERT INTO MAJOR VALUES ("QF", "Quantitative Finance", "CHS");
+INSERT INTO MAJOR VALUES ("STATS", "Statistics", "CHS");
+
+INSERT INTO Major VALUES ("BIZ", "Business Administration (Accountancy)", "BIZ");
+INSERT INTO Major VALUES ("ACC", "Business Administration", "BIZ");
+
+INSERT INTO Major VALUES ("BZA", "Business Analytics", "COM");
+INSERT INTO Major VALUES ("CS", "Computer Science", "COM");
+INSERT INTO Major VALUES ("ISec", "Information Security", "COM");
+INSERT INTO Major VALUES ("IS", "Information Systems", "COM");
+INSERT INTO Major VALUES ("CEG", "Computer Engineering", "COM");
+
+INSERT INTO Major VALUES ("DEN", "Dentistry", "DEN");
+
+INSERT INTO Major VALUES ("ARCH", "Architecture", "DE");
+INSERT INTO Major VALUES ("ID", "Industrial Design", "DE");
+INSERT INTO Major VALUES ("LA", "Landscape Architecture", "DE");
+INSERT INTO Major VALUES ("PFM", "Project & Facilities Management", "DE");
+INSERT INTO Major VALUES ("RE", "Real Estate", "DE");
+
+INSERT INTO Major VALUES ("BME", "Biomedical Engineering", "ENGR");
+INSERT INTO Major VALUES ("CME", "Chemical Engineering", "ENGR");
+INSERT INTO Major VALUES ("CE", "Civil Engineering", "ENGR");
+INSERT INTO Major VALUES ("ES", "Engineering Science", "ENGR");
+INSERT INTO Major VALUES ("EnE", "Environmental Engineering", "ENGR");
+INSERT INTO Major VALUES ("EE", "Electrical Engineering", "ENGR");
+INSERT INTO Major VALUES ("ISE", "Industrial and Systems Engineering", "ENGR");
+INSERT INTO Major VALUES ("MSE", "Material Science & Engineering", "ENGR");
+INSERT INTO Major VALUES ("MS", "Mechanical Science", "ENGR");
+# INSERT INTO Major VALUES ("CEG", "Computer Engineering", "ENGR");	# *:* relationship
+
+INSERT INTO Major VALUES ("LAW", "Law", "LAW");
+
+INSERT INTO Major VALUES ("MED", "Medicine", "MED");
+
+INSERT INTO Major VALUES ("NSG", "Nursing", "NSG");
+
+INSERT INTO Major VALUES ("PHAR", "Pharmacy", "PHAR");
+
+INSERT INTO Major VALUES ("MUS", "Music", "MUS");
+
+
+# error code: 1175
+SET SQL_SAFE_UPDATES = 0;
+DELETE FROM Major;
+SET SQL_SAFE_UPDATES = 1;
