91cd511fe7a1b9f3739baf6da6f5d92c97d36cf8
Claire Lim <e0559833@u.nus.edu>
2021-06-08 17:35:31 +0800

    add MYSQL scripts for category, tag

---
+++ backend_orbital/MySQLscripts/insertCategory.sql
@@ -0,0 +1,2 @@
+INSERT INTO Category VALUES ("ACAD", "Academic related");
+INSERT INTO Category VALUES ("NON-ACAD", "Non-academic related");
---
+++ backend_orbital/MySQLscripts/insertTag.sql
@@ -0,0 +1,6 @@
+INSERT INTO Tag VALUES ("Module Registration", "Module Registration", "ACAD");
+INSERT INTO Tag VALUES ("S/U", "S/U", "ACAD");
+# note: figure out how to import data on modules from nusmods
+
+INSERT INTO Tag VALUES ("Interest Groups", "Interest Groups", "NON-ACAD");
+INSERT INTO Tag VALUES ("Campus Life", "Campus Life", "NON-ACAD");
