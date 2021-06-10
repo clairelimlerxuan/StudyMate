697814ef42bb773c3a2b85b97e8810286d948508
vjo-anne209 <vjoanne.1807@gmail.com>
2021-06-11 05:08:59 +0800

    No changes

--- frontend_orbital/src/pages/home.js
+++ frontend_orbital/src/pages/home.js
@@ -2,14 +2,12 @@ import React, { useState } from "react";
 import Navbar  from '../components/navbar'; 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import '../App.css';
-import IntroSection from '../components/intro-section';
+
 import Card from '../components/card';
 import Footer from '../components/footer';
 function Home() {
     return (
         <>
-
-            <IntroSection/>
             <Card/>
             <Footer/>
         </>
